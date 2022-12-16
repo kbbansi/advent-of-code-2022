e98815908927ab5aa0225d260ea80a7d2d875d08
kbbansi <kwabenaampofo5@gmail.com>
2022-12-16 16:33:24 +0000

    add file reader approach

--- src/main/java/org/example/dayone/CalorieCounting.java
+++ src/main/java/org/example/dayone/CalorieCounting.java
@@ -1,7 +1,6 @@
 package org.example.dayone;
 
-import org.example.Main;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,12 +29,34 @@ public class CalorieCounting {
             }
         }
         var maxCalorie = Collections.max(elfCalorieList);
+        System.out.println("Largest Calorie: " + maxCalorie);
         Collections.sort(elfCalorieList);
 
         var sumOfLargestThreeCalories = elfCalorieList.subList(Math.max(elfCalorieList.size() - 3, 0), elfCalorieList.size())
                 .stream()
                 .reduce(0, Integer::sum);
-        System.out.println(sumOfLargestThreeCalories);
+        System.out.println("Sum of Largest Calories: " + sumOfLargestThreeCalories);
         return maxCalorie;
+    }
+
+
+
+    /**
+     * <p>This approach reads input from a file</p>
+     * @param filePath path to file to be read
+     * @return maximum calories
+     */
+    public static int calorieCounterFileReader(String filePath) throws IOException {
+        File file = new File(filePath);
+        StringBuilder fileContents = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String lines;
+
+        while ((lines = reader.readLine()) != null) {
+            fileContents.append(lines);
+            fileContents.append("\n");
+        }
+        reader.close();
+        return calorieCounterPartOne(fileContents.toString());
     }
 }
